eb264a3feaea00fb6327019b92d600a3ad703a59
pete201 <peterdeeming@hotmail.com>
2022-12-27 09:17:50 +0000

    sets neighboring cells in skirmishes

--- 2022 paper/Q2 v2.py
+++ 2022 paper/Q2 v2.py
@@ -1,3 +1,5 @@
+# https://www.olympiad.org.uk/papers/2022/bio/bio22-exam.pdf
+#
 # in this version i'll use a linear run of 25 hexagons and a look up table for neighbours
 # problem is I can't find a quick easy soln to find neighbors from a given hex (else a grid makes more sense)
 
@@ -5,30 +7,72 @@
 RED_MOVE_DISTANCE  = 9
 BLUE_MOVE_DISTANCE = 3
 
-neighbours = {
-    [0,1]:[1,4],
-    [0,2]:[5,5],
-}
 
 class hexagon():
     '''the building block of hive with 6 sides that can be set to red/blue'''
-
     def __init__(self) -> None:
-
         self.hex_controller = None
-
         self.sides_list = []
         for n in range (6):
             side = None
             self.sides_list.append(side)
 
+
     def get_controller(self):
+        '''returns team with majority sides or None'''
+        controller = "None"
+        if self.sides_list.count("R") > self.sides_list.count("B"):
+            controller = "R"
+        if self.sides_list.count("B") > self.sides_list.count("R"):
+            controller = "B"
+        return controller
+
+
+    def __str__(self) -> str:
+        rep = ""
         for item in self.sides_list:
-            print (item, end=" ")
-        print()
+            rep += str(item) + " "
+        return rep
+
 
 class hive():
     '''a set of 25 hexagons'''
+
+    # neighbours is a list of all 25 hexagons, each with a list of neighbours per side
+    # NOTE look up cells in range 0-24, and sides in range 0-5
+    # NOTE returned cell number is in range 1-25 
+    # e.g cell=0 (first list), side=1 (second value in sublist) returns cell 2 as neighbour
+    neighbours = [
+        [None,2,6,None,None,None],
+        [None,3,7,6,1,None],
+        [None,4,8,7,2,None],
+        [None,5,9,8,3,None],
+        [None,None,10,9,4,None],
+        
+        [2,7,12,11,None,1],
+        [3,8,13,12,6,2],
+        [4,9,14,13,7,3],
+        [5,10,15,14,8,4],
+        [None,None,None,13,9,5],
+
+        [6,12,16,None,None,None],
+        [7,13,17,16,11,6],
+        [8,14,18,17,12,7],
+        [9,15,19,18,11,8],
+        [10,None,20,19,14,9],
+
+        [12,17,22,21,None,11],
+        [13,18,23,22,16,12],
+        [14,19,24,23,17,13],
+        [15,20,25,24,18,14],
+        [None,None,None,25,19,15],
+
+        [16,22,None,None,None,None],
+        [17,23,None,None,21,16],
+        [18,24,None,None,22,17],
+        [19,25,None,None,23,18],
+        [20,None,None,None,24,19]
+        ]
 
     def __init__(self) -> None:
         self.hex_list = []
@@ -36,18 +80,35 @@ class hive():
             hex = hexagon()
             self.hex_list.append(hex)
 
+
     def get_hex_controller(self, ref):
-        self.hex_list[ref].get_controller()
+        '''returns team with majority sides or None'''
+        return self.hex_list[ref].get_controller()
+
 
     def set_hex_side(self, hex, side, team):
+        '''sets contorol of hexagon sides when a bee lands'''
         self.hex_list[hex].sides_list[side] = team
         # and also set the corresponding side of neighbouring hexagons
+        hex_neighbour = hive.neighbours[hex][side] # -1 see neighbour NOTE
+        # neighbour's side is opposite side 
+        side_neighbour = side + 3
+        if side_neighbour > 5:
+            side_neighbour -= 6
+        if hex_neighbour:
+            self.hex_list[int(hex_neighbour) -1].sides_list[side_neighbour] = team
 
+
+    def __str__(self) -> str:
+        '''returns a representation of the hive status'''
+        rep = ""
+        for hex in range (25):
+            rep += str(self.hex_list[hex]) + "\n"
+        return rep
 
 
 class bee():
     '''buzzes around the hive setting sides to it's team colour'''
-
     def __init__(self, start_hex, start_direction, rotation, move_distance) -> None:
         self.position = start_hex
         self.move_distance = move_distance
@@ -56,6 +117,7 @@ class bee():
 
     
     def rotate_move(self, move):
+        '''rotates the bee and moves it's set move distance'''
         self.direction += self.rotation
         if self.direction > 5:
             self.direction = 0
@@ -79,21 +141,26 @@ def skirmish():
     myHive.set_hex_side(blue.position,blue.direction, 'B') 
     blue.rotate_move(BLUE_MOVE_DISTANCE)
     
-
+####################
+# Main starts here #
+####################
 
 myHive = hive()
 
 "Two drones are in the hive: a red drone on hexagon 1 facing edge 1" 
 "and a blue drone on hexagon 25 facing edge 6."
-red = bee(0, 0, 1, RED_MOVE_DISTANCE)
-blue = bee(24, 5, -1, BLUE_MOVE_DISTANCE)
+red = bee(start_hex=0, start_direction=0, rotation=1, move_distance=RED_MOVE_DISTANCE)
+blue = bee(start_hex=24, start_direction=5, rotation=-1, move_distance=BLUE_MOVE_DISTANCE)
 
 for n in range(3):
     skirmish()
 
-# print out the hive:
-for n in range (25):
-        print(n+1, end=" ")
-        myHive.get_hex_controller(n)
+print("print hive representation")
+print(myHive)
 
-print(neighbours([0,1]))
+print("Hive hex controller summary")
+
+for n in range (25):
+    print(n+1, end=" ")
+    print(myHive.get_hex_controller(n))
+
