64adc48f8bf90c426676394a40b73af5371b9552
pete201 <peterdeeming@hotmail.com>
2023-01-23 21:41:40 +0000

    todo - change drone move to coods

--- 2022/Q2 v3.py
+++ 2022/Q2 v3.py
@@ -52,9 +52,36 @@ class hive():
         rep = ""
         for r in range(len(self.rows)):
             for c in range (len(self.hexagons)):
-                rep += str(self.rows[int(r)][int(c)]) + "\t"
+                rep += str(self.rows[r][c]) + "\t"
             rep += "\n"
         return rep
+
+
+class drone():
+    '''buzzes round the hive setting sides as it goes'''
+    def __init__(self, name, start_coords, start_direction, rotation, move_distance) -> None:
+        self.name = name
+        self.position = start_coords
+        self.move_distance = move_distance
+        self.direction = start_direction
+        self.rotation = rotation
+
+    
+    def rotate_move(self, move):
+        '''rotates the bee and moves it's set move distance'''
+        self.direction += self.rotation
+        if self.direction > 5:
+            self.direction = 0
+        if self.direction < 0:
+            self.direction = 5
+
+        #TODO - change this to co-ordinates system
+        #  self.position += self.move_distance
+        # if self.position > 24:
+        #     self.position -= 25 #25 hexagons in the hive
+        # if self.position < 0:
+        #     self.position += 25
+
 
 ###################################################################################
 #  MAIN  #
