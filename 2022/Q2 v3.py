6843b7dd0399db806570370dde6bdefdaa9f4ea6
pete201 <peterdeeming@hotmail.com>
2023-02-04 19:08:13 +0000

    still not correct results

--- 2022/Q2 v3.py
+++ 2022/Q2 v3.py
@@ -2,11 +2,15 @@
 #
 # in this version i'll use a 5x5 array of hexagons attempting to find neighbours from a given hex
 
+
+#TODO failed on 25,15,3,13 (should be 10,13)
+#TODO failed on 25,24,11,33 (should be 7,16)
+
 # contants
-RED_MOVE_DISTANCE  = 9
-BLUE_MOVE_DISTANCE = 3
-SKIRMISHES = 24
-FEUDS = 0
+RED_MOVE_DISTANCE  = 18
+BLUE_MOVE_DISTANCE = 6
+SKIRMISHES = 53
+FEUDS = 3
 
 class hexagon():
     '''the building block of hive with 6 sides that can be set to red/blue'''
@@ -64,14 +68,14 @@ class hive():
                 neighbour[0] = (drone_coords[0] - 1)
                 neighbour[1] = drone_coords[1] + drone_coords[0]%2
                 if neighbour[1] < 5:
-                    print(drone_coords,'found a neighbour at', neighbour)
+                    #print(drone_coords,'found a neighbour at', neighbour)
                     return neighbour
         # RIGHT:
         if drone_direction == 1:
             neighbour[0] = drone_coords[0]
             if drone_coords[1] < 4:
                 neighbour[1] = drone_coords[1] + 1
-                print(drone_coords,'found a neighbour at', neighbour)
+                #print(drone_coords,'found a neighbour at', neighbour)
                 return neighbour
         # BOTTOM RIGHT:
         if drone_direction == 2:
@@ -79,7 +83,7 @@ class hive():
                 neighbour[0] = (drone_coords[0] + 1)
                 neighbour[1] = drone_coords[1] + drone_coords[0]%2
                 if neighbour[1] < 5:
-                    print(drone_coords,'found a neighbour at', neighbour)
+                    #print(drone_coords,'found a neighbour at', neighbour)
                     return neighbour
         # BOTTOM LEFT:
         if drone_direction == 3:
@@ -87,14 +91,14 @@ class hive():
                 neighbour[0] = (drone_coords[0] + 1)
                 neighbour[1] = drone_coords[1] -1 + drone_coords[0]%2
                 if neighbour[1] > -1:
-                    print(drone_coords,'found a neighbour at', neighbour)
+                    #print(drone_coords,'found a neighbour at', neighbour)
                     return neighbour
         # LEFT:
         if drone_direction == 4:
             neighbour[0] = drone_coords[0]
             if drone_coords[1] > 0:
                 neighbour[1] = drone_coords[1] - 1
-                print(drone_coords,'found a neighbour at', neighbour)
+                #print(drone_coords,'found a neighbour at', neighbour)
                 return neighbour
         # TOP LEFT:
         if drone_direction == 5:
@@ -102,7 +106,7 @@ class hive():
                 neighbour[0] = (drone_coords[0] - 1)
                 neighbour[1] = drone_coords[1] -1 + drone_coords[0]%2
                 if neighbour[1] > -1:
-                    print(drone_coords,'found a neighbour at', neighbour)
+                    #print(drone_coords,'found a neighbour at', neighbour)
                     return neighbour
 
         return None
@@ -137,6 +141,14 @@ class drone():
         self.move_distance = move_distance
         self.direction = start_direction
         self.rotation = rotation
+        self.feud_hex_start = start_coords[0]
+        if self.feud_hex_start == 0:
+            self.feud_hex_stop = 5
+        else: self.feud_hex_stop = -1
+        self.feud_line_start = start_direction
+        if self.feud_line_start == 0:
+            self.feud_line_stop = 6
+        else: self.feud_line_stop = -1
 
     def rotate_move(self):
         '''rotates the bee and moves it's set move distance'''
@@ -164,6 +176,90 @@ def skirmish(n):
         blue.rotate_move()
         print(red.position, blue.position)
 
+    
+def feud(drone, opponent):
+    # preff is: +2 hexagons, +1 hex -1 opponent hex, +1 hex, -1 opponent hex, any unoccupied side
+
+    #list neutral hexagons with an unoccupied side
+    neutral_avail = []
+    for row in range(drone.feud_hex_start , drone.feud_hex_stop, drone.rotation):
+        for hex in range(drone.feud_hex_start , drone.feud_hex_stop, drone.rotation):
+            hive_hex = [row, hex]
+            if myHive.get_side_count(hive_hex, drone) == myHive.get_side_count(hive_hex, opponent):
+                # look for an unoccupied side, i.e. == '_'
+                for side in range(drone.feud_line_start, drone.feud_line_stop, drone.rotation):
+                    if myHive.rows[row][hex].sides[side] == '_':
+                        #print(f'neutral hex {hive_hex} has free side at {side}')
+                        neutral_avail.append(hive_hex)
+                        break
+    # +2 hexagons: - must have neutral neighbour
+    for each in neutral_avail:
+        # look for an unoccupied side, i.e. == '_'
+        for side in range(drone.feud_line_start, drone.feud_line_stop, drone.rotation):
+            if myHive.rows[each[0]][each[1]].sides[side] == '_':
+                neighbour = myHive.get_neighbour(each, side)
+                if neighbour:
+                    if myHive.get_side_count(neighbour, drone) == myHive.get_side_count(neighbour, opponent):
+                        print(f'feud: {drone.name} takes {each} side {side} AND neutral neighbour {neighbour}')
+                        myHive.set_hexagon_side(each, side, drone.name)
+                        myHive.set_opposite_side(neighbour, side, drone.name)
+                        return
+    # now look for +1 hex -1 opponent hex...
+    for each in neutral_avail:
+        # look for an unoccupied side, i.e. == '_'
+        for side in range(drone.feud_line_start, drone.feud_line_stop, drone.rotation):
+            if myHive.rows[each[0]][each[1]].sides[side] == '_':
+                neighbour = myHive.get_neighbour(each, side)
+                if neighbour:
+                    if myHive.get_side_count(neighbour, drone) + 1 == myHive.get_side_count(neighbour, opponent):
+                        print(f'feud: {drone.name} takes {each} side {side} and neutralises opponent neighbour {neighbour}')
+                        myHive.set_hexagon_side(each, side, drone.name)
+                        myHive.set_opposite_side(neighbour, side, drone.name)
+                        return
+    # now look for +1 hex 
+    for each in neutral_avail:
+        # look for an unoccupied side, i.e. == '_'
+        for side in range(drone.feud_line_start, drone.feud_line_stop, drone.rotation):
+            if myHive.rows[each[0]][each[1]].sides[side] == '_':
+                print(f'feud: {drone.name} takes {each} side {side} to take single hexagon')
+                myHive.set_hexagon_side(each, side, drone.name)
+                #if this side has a neighbour, set that too
+                neighbour = myHive.get_neighbour(each, side)
+                if neighbour:
+                    myHive.set_opposite_side(neighbour, side, drone.name)
+                    return
+    # now look for -1 opponent hex 
+    for row in range(drone.feud_hex_start , drone.feud_hex_stop, drone.rotation):
+        for hex in range(drone.feud_hex_start , drone.feud_hex_stop, drone.rotation):
+            hive_hex = [row, hex]
+            if myHive.get_side_count(hive_hex, drone) +1 == myHive.get_side_count(hive_hex, opponent):
+                # look for an unoccupied side, i.e. == '_'
+                for side in range(drone.feud_line_start, drone.feud_line_stop, drone.rotation):
+                    if myHive.rows[row][hex].sides[side] == '_':
+                        print(f'feud: {drone.name} found unoccupied {hive_hex} side {side} to neutralise opponent hexagon')
+                        myHive.set_hexagon_side(hive_hex, side, drone.name)
+                        #if this side has a neighbour, set that too
+                        neighbour = myHive.get_neighbour(hive_hex, side)
+                        if neighbour:
+                            myHive.set_opposite_side(neighbour, side, drone.name)
+                            return
+
+    # lastly, take first unoccupied side:
+    for row in range(drone.feud_hex_start , drone.feud_hex_stop, drone.rotation):
+        for hex in range(drone.feud_hex_start , drone.feud_hex_stop, drone.rotation):
+            hive_hex = [row, hex]
+            # look for an unoccupied side, i.e. == '_'
+            for side in range(drone.feud_line_start, drone.feud_line_stop, drone.rotation):
+                if myHive.rows[row][hex].sides[side] == '_':
+                    print(f'feud: {drone.name} found unoccupied {hive_hex} side {side} as a last resort....')
+                    myHive.set_hexagon_side(hive_hex, side, drone.name)
+                    #if this side has a neighbour, set that too
+                    neighbour = myHive.get_neighbour(hive_hex, side)
+                    if neighbour:
+                        myHive.set_opposite_side(neighbour, side, drone.name)
+                        return
+    print(f'feud: {drone.name}  NOTHING FOUND')
+
 ###################################################################################
 #  MAIN  #
 
@@ -174,7 +270,7 @@ blue = drone('B', [4,4], 5, -1, BLUE_MOVE_DISTANCE)
 
 skirmish(SKIRMISHES)
     
-print(myHive)
+#print(myHive)
 
 red_control_count = 0
 blue_control_count = 0
@@ -186,5 +282,26 @@ for row in range(5):
         if myHive.get_side_count([row,col], blue) > myHive.get_side_count([row,col], red):
             blue_control_count +=1
 
+print(f'after {SKIRMISHES} skirmishes...')
 print('Red controlls : ', red_control_count)
 print('Blue controlls: ', blue_control_count)
+
+for f in range(FEUDS):
+    feud(red, blue)
+    feud(blue, red)
+
+print(myHive)
+
+red_control_count = 0
+blue_control_count = 0
+for row in range(5):
+    for col in range(5):
+        if myHive.get_side_count([row,col], red) > myHive.get_side_count([row,col], blue):
+            red_control_count +=1
+        if myHive.get_side_count([row,col], blue) > myHive.get_side_count([row,col], red):
+            blue_control_count +=1
+
+print(f'after {FEUDS} feuds...')
+print('Red controlls : ', red_control_count)
+print('Blue controlls: ', blue_control_count)
+
