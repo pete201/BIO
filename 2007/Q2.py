e5a2e5069560376350d563ca652a71c027935e81
pete201 <peterdeeming@hotmail.com>
2023-02-06 21:24:03 +0000

    refactored

--- 2007/Q2.py
+++ 2007/Q2.py
@@ -8,31 +8,21 @@ if debug:
     #board = 'oeooxxxxo'    # O wins
     board = 'xxexooxxx'    # X wins
     board = board.upper()
-
 else:
     input_string = input('Enter valid start posn (e.g. EOOOOXXXX)')
     board = input_string.upper()
 
-# turn string into list so we can manipulate it
-# board_list = []
-# for char in board:
-#     board_list.append(char)
-
 # initial condition
 player = 'O'
 opponent = 'X'
-count = 0
 MAX_TRIES = 19
+count = 0
 
 def swap_player(player):
     if player == 'O':
-        player = 'X'
-        opponent = 'O'
+        return 'X', 'O'
     else: 
-        player = 'O'
-        opponent = 'X'
-
-    return player, opponent
+        return 'O', 'X'
 
 
 def check_win(board, player):
@@ -40,15 +30,10 @@ def check_win(board, player):
     putahi = board[0]
     kawai = board[1:]
     pad_kawai = kawai[-1] + kawai + kawai[0]
-    win = False
-    #check puthai
-    if putahi == player:
-        # now check for PEP sequence
-        # find returns position of string, or -1 if not found
-        if pad_kawai.find(player + 'E' + player) > -1:
-            win = True
-    return win
-
+    #check player occupies puthai and check for P-E-P sequence 
+    # (find returns position of string, or -1 if not found)
+    return (putahi == player) and (pad_kawai.find(player + 'E' + player) > -1)
+    
 
 ######################main ###########################
 print(f'initial board layout is ', board)
@@ -61,6 +46,8 @@ while count < MAX_TRIES:
 
     player, opponent = swap_player(player)
     count += 1
+
+    # player takes a turn
 
 if count < MAX_TRIES:
     print(f'{player} wins with board {board}')
