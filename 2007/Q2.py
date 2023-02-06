b3f1158c7db092a531c9cc2cc2cffafe20620523
pete201 <peterdeeming@hotmail.com>
2023-02-06 21:06:29 +0000

    test win is working ok.  1Hr

--- 2007/Q2.py
+++ 2007/Q2.py
@@ -1,17 +1,69 @@
-# input 9 characters
 
-debug = False
+
+# input 9 characters
+debug = True
 
 if debug:
-    input_string = 'EOOOOXXXX'
+    #board = 'EOOOOXXXX'
+    #board = 'oeooxxxxo'    # O wins
+    board = 'xxexooxxx'    # X wins
+    board = board.upper()
+
 else:
     input_string = input('Enter valid start posn (e.g. EOOOOXXXX)')
-    input_string = input_string.upper()
+    board = input_string.upper()
 
 # turn string into list so we can manipulate it
-input_list = []
-for char in input_string:
-    input_list.append(char)
+# board_list = []
+# for char in board:
+#     board_list.append(char)
 
-#test
-print(f'initial board layout is ', input_list)
+# initial condition
+player = 'O'
+opponent = 'X'
+count = 0
+win = False
+MAX_TRIES = 19
+
+def swap_player(player):
+    if player == 'O':
+        player = 'X'
+        opponent = 'O'
+    else: 
+        player = 'O'
+        opponent = 'X'
+
+    return player, opponent
+
+
+def check_win(board, player):
+    # to win player must occupy putahi (centre) and sequence P-E-P in kawai
+    putahi = board[0]
+    kawai = board[1:]
+    pad_kawai = kawai[-1] + kawai + kawai[0]
+    win = False
+    #check puthai
+    if putahi == player:
+        # now check for PEP sequence
+        # find returns position of string, or -1 if not found
+        if pad_kawai.find(player + 'E' + player) > -1:
+            win = True
+    return win
+
+
+######################main ###########################
+print(f'initial board layout is ', board)
+
+
+while not win and count < MAX_TRIES:
+    if check_win(board, player=player):
+        break
+    else: print(f'No winner: {player} has {board}')
+
+    player, opponent = swap_player(player)
+    count += 1
+
+
+if count < MAX_TRIES:
+    print(f'{player} wins with board {board}')
+else: print('Its a draw - try again')
