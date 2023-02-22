0db7e7478073d2a1190905d37ec652c09da774dc
pete201 <peterdeeming@hotmail.com>
2023-02-22 21:55:46 +0000

    general tidy

--- 2008/Q2.py
+++ 2008/Q2.py
@@ -3,13 +3,14 @@
 '''
 use class for rotors:
     Left port (input), + offset = right port (output)
-    has 'rotate' method which rotates the offset
+    has 'turn' method which rotates the offset
     class has a rotate counter which is used to rotate next rotor
 and a class for reflector:
     input translates to output
 '''
 
 debug = False
+PORTS = 'ABCD'
 
 if debug:
     encrypted_letters = 14
@@ -20,17 +21,14 @@ else:
     word = word.upper()
 
 class rotor():
-    '''offsets input character in both left-right and right-left directions
-    and rotates offset after each use'''
-
-    PORTS = 'ABCD'
+    '''offsets input character in both left-right and right-left directions'''
 
     def __init__(self, off0, off1, off2, off3) -> None:
         '''set up the rotors offsets which are unique to each rotor'''
         self.left_offset_ref = [off0, off1, off2, off3]
         self.left_offset = self.left_offset_ref
         # set up right hand side based on LHS. 
-        # basis is that LH_index+LtoR_value = RH_index
+        # basis is that LH_index + LtoR_value = RH_index
         # and now RtoL_value at RH-index = -LtoR_value
         # e.g. B=1, left_offset[1] = 1 : RH = 2 = C
         # so C = 2 has RtoL_value of -1
@@ -43,87 +41,54 @@ class rotor():
 
     def left_port(self, char_in):
         '''reads an input on left and outputs right port'''
-        index = rotor.PORTS.find(char_in)
+        index = PORTS.find(char_in)
         translation = (index + self.left_offset[index])%4
-        return rotor.PORTS[translation]
+        return PORTS[translation]
 
     def right_port(self, char_in):
         '''reads an input on right and outputs left port'''
-        index = rotor.PORTS.find(char_in)
+        index = PORTS.find(char_in)
         translation = (index + self.right_offset[index])%4
-        return rotor.PORTS[translation]
+        return PORTS[translation]
 
     def turn(self, n):
         '''rotates both the right and left port translations'''
         n = n%4
+        # note that we always calculate from the static '_ref' arrays
         self.left_offset = self.left_offset_ref[n:] + self.left_offset_ref[:n]
-        self.right_offset = self.right_offset_ref[n:] + self.right_offset_ref[:n]
-        
+        self.right_offset = self.right_offset_ref[n:] + self.right_offset_ref[:n] 
         #print(f'rotor_turn: left_offset = {self.left_offset}, right_offset = {self.right_offset}')
         return
 
 class reflector():
     '''input translates to output'''
-
-    PORTS = 'ABCD'
-
     def __init__(self) -> None:
         '''Only one static reflector, so set up here'''
         self.offset = [3, 1, -1, -3]
 
     def reflect(self, char_in):
-        index = reflector.PORTS.find(char_in)
+        index = PORTS.find(char_in)
         translation = (index + self.offset[index])
-        return reflector.PORTS[translation]
+        return PORTS[translation]
 
 
 def main():
     global encrypted_letters
     encrypted_word = ''
+
     rotor1 = rotor(0, 2, -1, -1)
     rotor2 = rotor(0, 1, -1, 0)
     reflector1 = reflector()
-
-    # step1 = rotor1.left_port('A')
-    # print(step1)
-    # step2 = rotor2.left_port(step1)
-    # print(step2)
-    # step3 = reflector1.reflect(step2)
-    # print(step3)
-    # step4 = rotor2.right_port(step3)
-    # print(step4)
-    # step5 = rotor1.right_port(step4)
-    # print(step5, end='\n\n')
-
-    # rotor1.turn(n)
-
-    # step1 = rotor1.left_port('A')
-    # print(step1)
-    # step2 = rotor2.left_port(step1)
-    # print(step2)
-    # step3 = reflector1.reflect(step2)
-    # print(step3)
-    # step4 = rotor2.right_port(step3)
-    # print(step4)
-    # step5 = rotor1.right_port(step4)
-    # print(step5, end='\n\n')
-
         
-
     for char in word:
-        # turns for first rotor is simply modulo n
         rotor1.turn(encrypted_letters)
-        # turns for second rotor is integer divide n, then modulo n
+        # turns for second rotor is 'integer-divide' encrypted_letters
         rotor2.turn(encrypted_letters//4)
 
         step1 = rotor1.left_port(char)
-        #print(step1)
         step2 = rotor2.left_port(step1)
-        #print(step2)
         step3 = reflector1.reflect(step2)
-        #print(step3)
         step4 = rotor2.right_port(step3)
-        #print(step4)
         step5 = rotor1.right_port(step4)
         #print(f'{char} becomes {step5}')
         encrypted_word += step5
@@ -131,7 +96,6 @@ def main():
         encrypted_letters += 1
 
     print(f'encrypted word is: {encrypted_word}')
-
 
 
 if __name__ == '__main__':
