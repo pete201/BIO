e9340b73c12143dbfa3efddd3273cb775d435e78
pete201 <peterdeeming@hotmail.com>
2023-04-12 10:52:04 +0100

    works a bit

--- 2008/Q3.py
+++ 2008/Q3.py
@@ -1,13 +1,12 @@
-# i'll use a string instead of a list
+# sort shirts on a line
+# works but not very well.  does not find optimum sort
+# e.g. 7123456 should take 2 moves, but my prog takes 4
 
 debug = False
-max_count = 100
+max_count = 50
 
-if debug:
-    washing_line = "6417352"
-else:
-    washing_line = input("enter 7 digit number: ")
-
+if debug: washing_line = "6417352"
+else: washing_line = input("enter 7 digit number: ")
 
 def leftmost(instring):
     return instring[1:4] + instring[0] + instring[4:] 
@@ -32,47 +31,46 @@ print(f'original = {washing_line}')
 
 def find_pair(instring):
     '''Looks for a move that results in a number pairing; returns 0 if pairing found, 1 if not'''
+    outstring = instring
     # test move 1 - leftmost to middle...
-    if abs(instring[0]-instring[3])==1 or abs(instring[0]-instring[4])==1:
-        leftmost(instring)
-        return 0 
+    if int(instring[0]) - int(instring[3]) == 1 or int(instring[0]) - int(instring[4]) == -1:
+        outstring = leftmost(instring)
     # test move 2 - rightmost to middle...
-    if abs(instring[6]-instring[2])==1 or abs(instring[0]-instring[3])==1:
-        rightmost(instring)
-        return 0
+    elif int(instring[6]) - int(instring[2])==1 or int(instring[6]) - int(instring[3]) == -1:
+        outstring = rightmost(instring)
     # test move 3 - middle to left...
-    if abs(instring[3]-instring[0])==1:
-        middle_left(instring)
-        return 0
+    elif int(instring[3]) - int(instring[0]) == -1:
+        outstring = middle_left(instring)
     # test move 4 - middle to right...
-    if abs(instring[3]-instring[6])==1:
-        middle_right(instring)
-        return 0
-    return 1 #pairing not found
+    elif int(instring[3]) - int(instring[6])==1:
+        outstring = middle_right(instring)
+    return outstring, instring != outstring
 
 def find_out_of_sequence(instring):
     '''Looks for first number that is not paired and moves it! '''
+    outstring = instring
     # test move 1 - leftmost to middle...
-    if abs(instring[0]-instring[1]) != 1:
-        leftmost(instring)
-        return
+    if int(instring[0]) - int(instring[1]) != -1:
+        outstring = leftmost(instring)
     # test move 2 - rightmost to middle...
-    if abs(instring[6]-instring[5]) != 1:
-        rightmost(instring)
-        return
+    elif int(instring[6]) - int(instring[5]) != 1:
+        outstring = rightmost(instring)
     # test move 3 - middle to left if value is <4...
-    if abs(instring[3]-instring[2])!=1 and abs(instring[3]-instring[4])!=1:
-        if instring[3] < 4:
-            middle_left(instring)
-        # DEFAULT move 4 - middle to left if value is >3...
-        else middle_right(instring)
-        return
+    elif int(instring[3]) - int(instring[2]) != 1 and int(instring[3]) - int(instring[4]) != -1:
+        if int(instring[3]) < 4:
+            outstring = middle_left(instring)
+        # DEFAULT move 4 - middle to right if value is >3...
+        else: outstring = middle_right(instring)
+    return outstring, instring != outstring
     
 
 count = 0
 while not (washing_line == '1234567') and count < max_count:
-    if not find_pair(washing_line):
-        find_out_of_sequence(washing_line)
+    washing_line, pair_found = find_pair(washing_line)
+    if not pair_found:
+        washing_line, seq_found = find_out_of_sequence(washing_line)
+        if not seq_found:
+            print('ERROR - how did we get here??')
     count += 1
     print(f'count = {count}, line = {washing_line}')
 
