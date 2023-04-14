a781d8ee20eb24ab2988a3fbed266c2ff8ec3b38
pete201 <peterdeeming@hotmail.com>
2023-04-14 15:36:39 +0100

    works but not all inpuuts

--- 2008/Q3 v2.py
+++ 2008/Q3 v2.py
@@ -1,9 +1,9 @@
 # sort shirts on a line
-# works but not very well.  does not find optimum sort
-# e.g. 7123456 should take 2 moves, but my prog takes 4
+# works but not very well.  does not find every sort
+# e.g. 5674321 is not found within 100 moves (should take 14)
 
 debug = False
-max_count = 10
+max_count = 100
 
 if debug: washing_line = "6417352"
 else: washing_line = input("enter 7 digit number: ")
@@ -20,31 +20,19 @@ def middle_left(instring):
 def middle_right(instring):
     return instring[0:3] + instring[4:] + instring[3]
  
-
-print(f'original = {washing_line}')
-
 # that's the easy bit done, now I need an algorithm
-
 # try:
-# perform each operation and see which result gives most chars in sequence
-# in sequence is counted for any digit which is adjacent to next in sequence
-# e.g. 7126345 would yield 5 in sequence digits
+# perform each operation and see which result gives most connections
+# a connection is counted for any digit which is adjacent to next in sequence
 
 def count_in_sequence(instring):
     count = 0
     i=0
-    for i in range(7):
-        if i < 6:
-            if int(instring[i]) == int(instring[i+1]) - 1:
-                count += 1
-            elif int(instring[i]) == int(instring[i-1]) + 1:
-                count += 1
-        elif i == 6:
-            if int(instring[i]) == int(instring[i-1]) + 1:
-                count += 1
-    #debug
-    print('count_in_sequence: ',instring,':',count )
-    #ENDdebug
+    for i in range(6):
+        if int(instring[i]) == int(instring[i+1]) - 1:
+            count += 1
+    if debug:
+        print('count_in_sequence: ',instring,':',count )
     return count
 
 def find_best_move(instring):
@@ -55,11 +43,16 @@ def find_best_move(instring):
     best = results.index(max(results))
     return moves[best]
 
+print(f'original = {washing_line}')
 count = 0
 while not (washing_line == '1234567') and count < max_count:
     next_move = find_best_move(washing_line)
     washing_line = next_move(washing_line)
     count += 1
-    print(f'count = {count}, line = {washing_line}')
+    if debug:
+        print(f'count = {count}, line = {washing_line}')
 
-print('completed in:', count)
+if washing_line == '1234567':
+    print('completed in:', count)
+else:
+    print('error, did not complete')
